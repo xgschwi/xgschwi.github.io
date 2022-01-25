01dc3f6f283d2fb37c88ef72d8e3314507fc51ab
xgschwi <xaviergschwind@gmail.com>
2022-01-25 09:20:36 -0500

    Adding husky, prettier, and lint

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { hydrate, render } from "react-dom";
-import "./styles.css"
+import React from 'react';
+import { hydrate, render } from 'react-dom';
+import './styles.css';
 
-const Index = () => {
-    return(
-        <div>
-            <h1 className="text-xl text-green-600">Website under construction :D</h1>
-            <p className="text-2xl text-red-600">Stay tuned</p>
-        </div>
-    )
+function Index() {
+	return (
+		<div>
+			<h1 className="text-xl text-green-600">Website under construction :D</h1>
+			<p className="text-2xl text-red-600">Stay tuned</p>
+		</div>
+	);
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 if (rootElement.hasChildNodes()) {
-  hydrate(<Index />, rootElement);
+	hydrate(<Index />, rootElement);
 } else {
-  render(<Index />, rootElement);
+	render(<Index />, rootElement);
 }
