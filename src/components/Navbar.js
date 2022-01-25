74e09b4bc57516ad224a16a17719a8958ebcf761
xgschwi <xaviergschwind@gmail.com>
2022-01-25 11:24:20 -0500

    Started Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+	return (
+		<div className="bg-dull-blue flex h-10">
+			<div className="navbarItem">Home</div>
+			<div className="navbarItem">About Xavier</div>
+			<div className="navbarItem">Programmer&apos;s Best Friend</div>
+		</div>
+	);
+}
+
+export default Navbar;
