163d7471e5b34befa9f841a96b481a5abda5aef0
xgschwi <xaviergschwind@gmail.com>
2022-01-24 21:24:23 -0500

    Changing app to use babel and webpack

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Index = () => {
+    return(
+        <div>
+            <h1>Website under construction :D</h1>
+            <p>Stay tuned</p>
+        </div>
+    )
+}
+ReactDOM.render(<Index />, document.getElementById("root"));
